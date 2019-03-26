a5e82d297dc4a899ab7a395082974609222a9ffd
jKostet <juho.kostet@helsinki.fi>
2019-03-26 15:36:59 +0200

    Add Users and login/logout

--- application/__init__.py
+++ application/__init__.py
@@ -3,24 +3,41 @@
 from flask import Flask
 app = Flask(__name__)
 
-# Tuodaan SQLAlchemy käyttöön
 from flask_sqlalchemy import SQLAlchemy
-# Käytetään tasks.db-nimistä SQLite-tietokantaa. Kolme vinoviivaa
-# kertoo, tiedosto sijaitsee tämän sovelluksen tiedostojen kanssa
-# samassa paikassa
-# WISHES.db
+# TODO: Rename wishes.db to makerspace.db etc,
+# as we probably need just one database with more tables
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wishes.db"
-# Pyydetään SQLAlchemyä tulostamaan kaikki SQL-kyselyt
+
+# SQLAlchemy print all SQL requests
 app.config["SQLALCHEMY_ECHO"] = True
 
-# Luodaan db-olio, jota käytetään tietokannan käsittelyyn
+# Create db object
 db = SQLAlchemy(app)
 
-# Luetaan kansiosta application tiedoston views sisältö
 from application import views
 
 from application.wishes import models
 from application.wishes import views
 
-# Luodaan lopulta tarvittavat tietokantataulut
+from application.auth import models
+from application.auth import views
+
+# Account login manager
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+# Create TABLES
 db.create_all()
