d91a0c3b77e21296391bdde5c84c04f1bf7c3aaa
jKostet <juho.kostet@helsinki.fi>
2019-03-28 15:50:30 +0200

    Use postgresql on Heroku

--- application/__init__.py
+++ application/__init__.py
@@ -4,12 +4,17 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-# TODO: Rename wishes.db to makerspace.db etc,
-# as we probably need just one database with more tables
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wishes.db"
 
-# SQLAlchemy print all SQL requests
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    # TODO: Rename wishes.db to makerspace.db etc,
+    # as we probably need just one database with more tables
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///wishes.db"
+    # SQLAlchemy print all SQL requests
+    app.config["SQLALCHEMY_ECHO"] = True
 
 # Create db object
 db = SQLAlchemy(app)
@@ -38,6 +43,8 @@ login_manager.login_message = "Please login to use this functionality"
 def load_user(user_id):
     return User.query.get(user_id)
 
-
-# Create TABLES
-db.create_all()
+try:
+    # Create TABLES
+    db.create_all()
+except:
+    pass
