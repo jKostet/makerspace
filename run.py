e542ddb77b17891c25912669c1403693fd229c46
jKostet <juho.kostet@helsinki.fi>
2019-03-28 17:16:43 +0200

    Add more logging to Heroku. Old db without account table should be gone.

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from application import app
 
+# heroku logs
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+# TODO: Remember to remove debug mode later
 if __name__ == '__main__':
 	app.run(debug=True)
