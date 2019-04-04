f6a61572a3f760063ab7295676c0e1b7aa17884f
jKostet <juho.kostet@helsinki.fi>
2019-04-04 21:52:28 +0300

    Users and Wishes extend Base model for id and timestamps.

--- application/wishes/models.py
+++ application/wishes/models.py
@@ -1,13 +1,14 @@
 from application import db
+from application.models import Base
 
-class Wish(db.Model):
+class Wish(Base):
     # id
-    id = db.Column(db.Integer, primary_key=True)
+    #id = db.Column(db.Integer, primary_key=True)
     # date created
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    #date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     # date modified, defaults to date created, but updates timestamp when modified
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
-    onupdate=db.func.current_timestamp())
+    #date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
+    #onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
     approved = db.Column(db.Boolean, nullable=False)
