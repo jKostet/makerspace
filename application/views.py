73119eb30133c5f56678ab50d9e042d06ecc0da2
Juho Kostet <juho.kostet@gmail.com>
2019-06-30 23:00:40 +0300

    Redirect main page to Wish List as there is no content

--- application/views.py
+++ application/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
 from application import app
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+	return redirect(url_for("wishes_index"))
+	# TODO: Add updates and other content to home page
+	#return render_template("index.html")
